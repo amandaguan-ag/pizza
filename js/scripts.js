d858a51000c8f8a5acd9741e11ebff28398dfbd4
amandaguan-ag <amandaguan1314@gmail.com>
2023-02-23 14:06:30 -0800

    simplify Business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,31 @@
-//BUSINESS LOGIC//
+//business Logic
 
-//Pizza Object Constructor
-function Pizza() {
-    this.pizzaSize = "";
-    this.baseCost = 0;
-    this.individToppingCost = 0;
-    this.toppingsArray = [];
-    this.numToppings = 1,
-    this.pizzaCost = 0;
-}
-
-//Pizza Object Methods
-Pizza.prototype.setBaseCost = function(){
-    if (this.pizzaSize === "small") {
-        this.baseCost = 10;
-    } else if (this.pizzaSize === "medium") {
-        this.baseCost = 12;
-    } else if (this.pizzaSize === "large") {
-        this.baseCost = 14;
-    } 
-    return this.baseCost;
-}
-
-Pizza.prototype.setIndividToppingCost = function(){
-    if (this.pizzaSize === "small") {
-        this.individToppingCost = 1;
-    } else if (this.pizzaSize === "medium") {
-        this.individToppingCost = 1.5;
-    } else if (this.pizzaSize === "large") {
-        this.individToppingCost = 2;
-    } 
-    return this.individToppingCost;
-}
-
-Pizza.prototype.setToppingsArray = function(){
-    this.toppingsArray = [];
-    for (i = 0; i < this.numToppings; i++) {
-        this.toppingsArray.push("topping");
+function Pizza(size, toppings) {
+    this.pizzaSize = size;
+    this.orderToppings = toppings;
+  }
+  
+  Pizza.prototype.calculatePrice = function () {
+    var totalPrice = 0;
+      if (this.pizzaSize === "extra-large") {
+        this.totalPrice = 10;
+      } else if (this.pizzaSize === "large") {
+        this.totalPrice = 8;
+      } else if (this.pizzaSize === "medium") {
+        this.totalPrice = 6;
+      } else {
+        this.totalPrice = 4;
+      }
+  
+      if (this.orderToppings === 4) {
+        this.totalPrice += 4;
+      } else if (this.orderToppings === 3) {
+        this.totalPrice += 3;
+      } else if (this.orderToppings === 2) {
+        this.totalPrice += 2;
+      } else if (this.orderToppings === 1) {
+        this.totalPrice += 1;
+      } else {
+      }
+      return this.totalPrice;
     }
-    return this.toppingsArray;
-}
