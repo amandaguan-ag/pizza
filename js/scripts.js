795930067e08d870bc9dae9f405b62ea93155b21
amandaguan-ag <amandaguan1314@gmail.com>
2023-02-23 16:34:53 -0800

    adapt js to the newly added extra large size

--- js/scripts.js
+++ js/scripts.js
@@ -6,25 +6,29 @@ function Pizza(name, size, toppings) {
   }
   
   Pizza.prototype.calculatePrice = function () {
-    let totalPrice = 0;
-    if (this.pizzaSize === "large") {
-      this.totalPrice = 8;
-    } else if (this.pizzaSize === "medium") {
-      this.totalPrice = 6;
-    } else {
-      this.totalPrice = 4;
-    }
+    var totalPrice = 0;
+      if (this.pizzaSize === "extra-large") {
+        this.totalPrice = 10;
+      } else if (this.pizzaSize === "large") {
+        this.totalPrice = 8;
+      } else if (this.pizzaSize === "medium") {
+        this.totalPrice = 6;
+      } else {
+        this.totalPrice = 4;
+      }
   
-    if (this.orderToppings === 3) {
-      this.totalPrice += 3;
-    } else if (this.orderToppings === 2) {
-      this.totalPrice += 2;
-    } else if (this.orderToppings === 1) {
-      this.totalPrice += 1;
-    } else {
+      if (this.orderToppings === 4) {
+        this.totalPrice += 4;
+      } else if (this.orderToppings === 3) {
+        this.totalPrice += 3;
+      } else if (this.orderToppings === 2) {
+        this.totalPrice += 2;
+      } else if (this.orderToppings === 1) {
+        this.totalPrice += 1;
+      } else {
+      }
+      return this.totalPrice;
     }
-    return this.totalPrice;
-  };
   
   // User Logic
   function handleFormSubmission(event) {
